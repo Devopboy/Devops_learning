d704be4408f9230b8ac4acd81146d8c5c045c320
AJ <ajaykt70@gmail.com>
2024-08-25 11:46:54 -0500

    created ec2 instance with terraform

---
+++ Terraform/Terraform_Basics/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.76.1"
+    }
+  }
+}
+ 
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_instance" {
+  ami           = "ami-066784287e358dad1"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Ec2_Terraform"
+  }
+}
